e4609fc767f2d855b17fc6b76813e56ac86177af
Rüdiger zu Dohna <snackbox@sinntr.eu>
2015-11-07 14:19:32 +0100

    added MementoRule and SystemPropertiesRule

---
+++ src/main/java/com/github/t1/deployer/tools/SystemPropertiesRule.java
@@ -0,0 +1,24 @@
+package com.github.t1.deployer.tools;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+import org.junit.rules.ExternalResource;
+
+public class SystemPropertiesRule extends ExternalResource {
+    private final Map<String, String> oldSystemProperties = new HashMap<>();
+
+    public void given(String name, Object value) {
+        String previousValue = System.setProperty(name, value.toString());
+        oldSystemProperties.put(name, previousValue);
+    }
+
+    @Override
+    public void after() {
+        for (Entry<String, String> entry : oldSystemProperties.entrySet())
+            if (entry.getValue() == null)
+                System.clearProperty(entry.getKey());
+            else
+                System.setProperty(entry.getKey(), entry.getValue());
+    }
+}
