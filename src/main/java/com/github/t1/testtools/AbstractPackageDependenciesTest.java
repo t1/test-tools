0ec3d3883d26e8e2a69c6e225aff27b1dd1dabe8
Rüdiger zu Dohna <snackbox@sinntr.eu>
2016-06-18 14:58:33 +0200

    print package dependencies

--- src/main/java/com/github/t1/testtools/AbstractPackageDependenciesTest.java
+++ src/main/java/com/github/t1/testtools/AbstractPackageDependenciesTest.java
@@ -1,19 +1,18 @@
 package com.github.t1.testtools;
 
-import static java.util.Arrays.*;
-import static org.junit.Assert.*;
-
-import java.nio.file.*;
-import java.util.*;
-import java.util.ArrayList;
-import java.util.stream.StreamSupport;
-
-import org.junit.*;
-
 import com.github.t1.graph.*;
 import com.sun.tools.classfile.*;
 import com.sun.tools.classfile.Dependency.*;
 import com.sun.tools.jdeps.*;
+import org.junit.*;
+
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.StreamSupport;
+
+import static java.util.Arrays.*;
+import static org.junit.Assert.*;
 
 public abstract class AbstractPackageDependenciesTest {
     static final Map<String, Set<String>> packageDependencies = new TreeMap<>();
@@ -25,12 +24,13 @@ public abstract class AbstractPackageDependenciesTest {
         Finder finder = Dependencies.getClassDependencyFinder();
 
         archive.reader().getClassFiles()
-                .forEach(classFile -> StreamSupport.stream(finder.findDependencies(classFile).spliterator(), false)
-                        .filter(dependency -> !isAnnotation(dependency))
-                        .filter(dependency -> !self(dependency))
-                        .forEach(dependency -> packageDependencies
-                                .computeIfAbsent(dependency.getOrigin().getPackageName(), key -> new TreeSet<>())
-                                .add(dependency.getTarget().getPackageName())));
+               .forEach(classFile -> StreamSupport
+                       .stream(finder.findDependencies(classFile).spliterator(), false)
+                       .filter(dependency -> !isAnnotation(dependency))
+                       .filter(dependency -> !self(dependency))
+                       .forEach(dependency -> packageDependencies
+                               .computeIfAbsent(dependency.getOrigin().getPackageName(), key -> new TreeSet<>())
+                               .add(dependency.getTarget().getPackageName())));
     }
 
     private static boolean self(Dependency dependency) {
@@ -67,22 +67,28 @@ public abstract class AbstractPackageDependenciesTest {
         packageDependencies.forEach((key, set) -> {
             Node<String> node = graph.findOrCreateNode(key);
             set.stream()
-                    .filter(target -> packageDependencies.containsKey(target))
-                    .forEach(target -> node.linkedTo(graph.findOrCreateNode(target)));
+               .filter(packageDependencies::containsKey)
+               .forEach(target -> node.linkedTo(graph.findOrCreateNode(target)));
         });
 
         graph.topologicalSort();
+
+        System.out.println("----------------------- dependencies:");
+        System.out.println(graph.toString());
+        System.out.println("-----------------------");
     }
 
     @Test
     public void shouldHaveOnlyDefinedDependencies() {
         List<String> unexpected = new ArrayList<>();
-        packageDependencies.keySet().stream()
-                .map(p -> Package.getPackage(p))
+        packageDependencies
+                .keySet().stream()
+                .map(Package::getPackage)
                 .forEach(sourcePackage -> {
                     List<String> allowed = dependenciesOf(sourcePackage);
                     String source = sourcePackage.getName();
-                    packageDependencies.get(source).stream()
+                    packageDependencies
+                            .get(source).stream()
                             .filter(target -> !source.equals(target))
                             .filter(target -> !allowed.contains(target))
                             .filter(target -> !isAlwaysAllowed(target))
@@ -93,7 +99,8 @@ public abstract class AbstractPackageDependenciesTest {
     }
 
     private boolean isAlwaysAllowed(String target) {
-        return getAlwaysAllowedPackages().stream()
+        return getAlwaysAllowedPackages()
+                .stream()
                 .anyMatch(pattern -> {
                     if (pattern.endsWith("*"))
                         return target.startsWith(pattern.substring(0, pattern.length() - 1));
@@ -105,14 +112,17 @@ public abstract class AbstractPackageDependenciesTest {
     @Test
     public void shouldHaveNoSpecifiedButUnrealizedDependencies() {
         List<String> unrealized = new ArrayList<>();
-        packageDependencies.keySet().stream()
-                .map(p -> Package.getPackage(p))
-                .forEach(sourcePackage -> {
-                    dependenciesOf(sourcePackage).stream()
-                            .filter(dependency -> !packageDependencies.get(sourcePackage.getName())
-                                    .contains(dependency))
-                            .forEach(target -> unrealized.add(sourcePackage + " -> " + target));
-                });
+        packageDependencies
+                .keySet().stream()
+                .map(Package::getPackage)
+                .forEach(sourcePackage ->
+                        dependenciesOf(sourcePackage)
+                                .stream()
+                                .filter(dependency -> !packageDependencies
+                                        .get(sourcePackage.getName())
+                                        .contains(dependency))
+                                .forEach(target -> unrealized.add(
+                                        sourcePackage + " -> " + target)));
         if (!unrealized.isEmpty())
             fail("unrealized dependencies:\n" + String.join("\n", unrealized));
     }
